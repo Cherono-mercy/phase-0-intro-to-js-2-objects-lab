c6ea276025d0b30ec0d954d35035bcfbe6c4c5a4
Mercy Cherono <cheronom142@gmail.com>
2023-10-10 10:57:13 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+// Defining an employee variable and assigning it an object using literal syntax
+let employee = {
+  name: "Sam",
+  stretAddress: "20200 Nairobi",
+};
+employee;
+
+//Non-destructively update the object values
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+  return {
+    ...employee,
+    [streetAddress]: "11 Broadway",
+  };
+}
+
+//Destructively update an object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee["streetAddress"] = "12 Broadway";
+  return employee;
+}
+
+//Non-destructively deleting an object property
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee };
+  delete newEmployee["name"];
+  return newEmployee;
+}
+
+//Destructively delete an object property
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee["name"];
+  return employee;
+}
